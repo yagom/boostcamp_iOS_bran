1011d86e5360028c3443fa1b4580564ae0b8f60a
JU HO YOON <Bran@JUui-MacBook-Pro.local>
2017-07-01 01:13:32 +0900

    create LoginPage

---
+++ week1/LoginPage/LoginPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by JU HO YOON on 2017. 7. 1..
+//  Copyright © 2017년 YJH Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
