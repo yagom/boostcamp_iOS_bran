f97eff7e5c76b554cd771d15fa95f3cdc4dee4df
YoonJuHo (Bran) <synecticle@gmail.com>
2017-07-25 14:00:41 +0900

    UPDATE add stackview

--- week4/Quiz/Quiz/ViewController.swift
+++ week4/Quiz/Quiz/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var questionLabel: UILabel!
+//    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     let questions: [String] = ["From What is cognac made?",
@@ -23,12 +27,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        self.currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        self.updateOffScreenLabel()
+    }
+    
+    private func updateOffScreenLabel(){
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.currentQuestionLabel.alpha = 0
+        
     }
 
     @IBAction func showNextQuestion(sender: AnyObject) {
@@ -37,13 +50,53 @@ class ViewController: UIViewController {
             currentQuestionIndex = 0
         }
         let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        self.currentQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        self.animateLabelTransitions()
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
+    }
+    
+    func animateLabelTransitions() {
+        view.layoutIfNeeded()
+        let screenWidth = view.frame.width
+        
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+//        UIView.animate(withDuration: 0.5,
+//                       delay: 0,
+//                       options: [.curveLinear],
+//                       animations: {
+//            self.currentQuestionLabel.alpha = 0
+//            self.nextQuestionLabel.alpha = 1
+//            self.view.layoutIfNeeded()
+//        }) { _ in
+//            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+//            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+//            
+//            self.updateOffScreenLabel()
+//        }
+        
+        // 동메달 과제: 스프링 애니메이션.
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.3,
+                       initialSpringVelocity: 0.1,
+                       options: [],
+                       animations: {
+//            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+            self.view.layoutIfNeeded()
+        }) { _ in
+            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+            
+            self.updateOffScreenLabel()
+        }
     }
 
 }
